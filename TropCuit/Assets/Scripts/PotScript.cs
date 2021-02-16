37bd1d9a63456091c6b870e7d8701f7448ecc026
Arthur Tisseront <artisseront@gmail.com>
2021-02-16 18:59:46 -0500

    Added progress bars to pot prefab

--- TropCuit/Assets/Scripts/PotScript.cs
+++ TropCuit/Assets/Scripts/PotScript.cs
@@ -6,11 +6,21 @@ public class PotScript : MonoBehaviour
 {
     //Tracks the progress of the pot
     float progress;
+    bool breadIn;
+    bool cheeseIn;
+    bool meatIn;
+
+
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        progress = 0;
+        meatIn = false;
+        breadIn = false;
+        cheeseIn = false;
+
+
     }
 
     // Update is called once per frame
