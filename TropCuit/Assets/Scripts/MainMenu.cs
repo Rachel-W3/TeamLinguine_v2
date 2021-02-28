0cf35aad708bae528c99d169da0c4f1934e9adc8
Jeremiah <jwc9655@rit.edu>
2021-02-28 14:50:02 -0500

    Adjusted Caught Scene
    Shortened the video that plays when the player is caught

--- TropCuit/Assets/Scripts/MainMenu.cs
+++ TropCuit/Assets/Scripts/MainMenu.cs
@@ -13,6 +13,6 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("KitchenTest");
     }
 }
