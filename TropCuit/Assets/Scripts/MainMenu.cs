ef6d7529039e5c0925d5b78714fd99880d90cfd2
Rachel Wong <rachelwong1f@gmail.com>
2021-03-01 11:34:26 -0500

    Reduced rat drag and fixed main menu skipping over instructions bug

--- TropCuit/Assets/Scripts/MainMenu.cs
+++ TropCuit/Assets/Scripts/MainMenu.cs
@@ -13,6 +13,6 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene("KitchenTest");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
